c8af95af5828fadcab906d77b2ffd3379970fbda
Shubhranka Varma <c-shubhranka.varma@amagi.com>
2023-03-28 16:08:47 +0530

    addel localtunnel

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 const ip = require('ip');
+const ngrok = require('ngrok');
+const localtunnel = require('localtunnel');
 // const client = redis.createClient({
 //     url: 'redis://redis-server'
 // })
 
 // client.connect().then(async connectedClient=>{
 //     console.log("connected to redis")
-    
+
 //     // if visits is not set, set it to 0
 //     if(!await client.get('visits'))
 //     await client.set('visits', 0);
@@ -20,20 +22,41 @@ const ip = require('ip');
 //         res.send('Number of visits is ' + value);
 //         client.set('visits', parseInt(value) + 1);
 //     });
-    
+
 //     app.listen(8081, () => {
 //         console.log('Listening on port 8081');
 //     });
 // })
 
-app.get('/', (req, res) => {
-    console.log("Helloo")
-    debugger;
-    res.send('Hello002asdf30');
-});
+// const url = ngrok.connect(8081).then(url => {
 
-const server = app.listen(0, () => {
-    const ipAddr = ip.address();
-    console.log(ipAddr + ":" + server.address().port);
+//     console.log("ngrok url: " + url);
 
+//     app.get('/', (req, res) => {
+//         console.log("Helloo")
+//         debugger;
+//         res.send('Hello002asdf30');
+//     });
+
+//     const server = app.listen(8081, () => {
+//         const ipAddr = ip.address();
+//         console.log(ipAddr + ":" + server.address().port);
+
+//     });
+
+// })
+
+const tunnel = localtunnel({ port: 8081, subdomain: 'myapp' }, function(err, tunnel) {
+    console.log("localtunnel url: " + tunnel.url);
+    app.get('/', (req, res) => {
+        console.log("Helloo")
+        debugger;
+        res.send('Hello002asdf30');
+    });
+
+    const server = app.listen(8081, () => {
+        const ipAddr = ip.address();
+        console.log(ipAddr + ":" + server.address().port);
+
+    });
 });
